50648aed31384787a143a056fd43e02581acca77
triksi254 <89916911+triksi254@users.noreply.github.com>
2023-07-29 13:44:11 +0300

    Maratestnet connection established...Eliminated use of Web3ReactProvider. Using Ethers and JSONRPCPROVIDER only

---
+++ notepad/pages/_app.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import ContractArtifact from "../../artifacts/contracts/Lock.sol/Lock.json";
+
+function MyApp({ Component, pageProps }) {
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // The network url for Mara testnet
+  const maraTestnetUrl = "https://testapi.mara.xyz/http";
+
+  // Our deployed contract address
+  const contractAddress = "0x4ffE69cd9ACd52961844c297318fe12697972F8f";
+
+  useEffect(() => {
+    async function initialize() {
+      try {
+        // Check if the ethers library is available
+        if (!ethers || !ethers.providers || !ethers.providers.JsonRpcProvider) {
+          throw new Error("Ethers library not available");
+        }
+
+        if (maraTestnetUrl) {
+          const provider = new ethers.providers.JsonRpcProvider(maraTestnetUrl);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(
+            contractAddress,
+            ContractArtifact.abi,
+            signer
+          );
+          setProvider(provider);
+          setSigner(signer);
+          setContract(contract);
+        } else {
+          setError("Network url is not available");
+        }
+      } catch (error) {
+        console.error(error);
+        setError("Failed to initialize" + error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    initialize();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <Component
+      {...pageProps}
+      provider={provider}
+      signer={signer}
+      contract={contract}
+    />
+  );
+}
+
+export default MyApp;
---
+++ notepad/pages/index.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { ethers } from "ethers";
+import NoteTokenABI from "../../artifacts/contracts/Lock.sol/Lock.json";
+
+const Home = () => {
+  const [notes, setNotes] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [metamaskConnected, setMetamaskConnected] = useState(false);
+
+  const connectToMetamask = useCallback(async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccount(accounts[0]);
+      setMetamaskConnected(true);
+    } catch (error) {
+      console.error(error);
+      setMetamaskConnected(false);
+    }
+  }, []);
+
+  const mintNote = useCallback(async () => {
+    if (!inputValue) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setError(null);
+
+      // Check if the ethers library is available
+      if (!ethers || !ethers.utils || !ethers.utils.parseEther) {
+        throw new Error("Ethers library not available");
+      }
+
+      const notePrice = ethers.utils.parseEther("0.1"); // Note price in ETH (0.1 ETH)
+      const addNoteTx = await contract.addNote(inputValue, notePrice);
+      await addNoteTx.wait();
+
+      setInputValue("");
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [inputValue, contract]);
+
+  const purchaseNote = useCallback(
+    async (noteId) => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const note = notes.find((note) => note.id === noteId);
+        if (!note || note.purchased) {
+          throw new Error("Note not available for purchase");
+        }
+
+        // Convert note.price to BigNumber using ethers.BigNumber.from()
+        const notePriceInWei = ethers.BigNumber.from(note.price);
+
+        // Send the correct amount of Ether (notePriceInWei) when calling the purchaseNote function
+        const purchaseTx = await contract.purchaseNote(noteId, {
+          value: notePriceInWei,
+        });
+        await purchaseTx.wait();
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [notes, contract]
+  );
+
+  useEffect(() => {
+    async function initialize() {
+      try {
+        if (window.ethereum) {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+          const contractAddress = "0x4ffE69cd9ACd52961844c297318fe12697972F8f";
+
+          const noteTokenContract = new ethers.Contract(
+            contractAddress,
+            NoteTokenABI.abi,
+            signer
+          );
+
+          setContract(noteTokenContract);
+
+          const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          setAccount(accounts[0]);
+
+          const noteCount = await noteTokenContract.balanceOf(accounts[0]);
+          const notePromises = [];
+
+          for (let i = 0; i < noteCount; i++) {
+            notePromises.push(noteTokenContract.getNoteByIndex(i));
+          }
+
+          const noteResults = await Promise.all(notePromises);
+
+          const notes = noteResults.map((note, index) => ({
+            id: index,
+            text: note.text,
+            price: note.price.toString(),
+            purchased: note.purchased,
+          }));
+
+          setNotes(notes);
+          setMetamaskConnected(true);
+        } else {
+          console.log("Metamask not found");
+        }
+      } catch (error) {
+        console.error(error);
+        setError("Failed to initialize" + " " + error.message);
+      }
+    }
+    initialize();
+  }, []);
+
+  return (
+    <>
+      <div>
+        {error && <p>Error: {error}</p>}
+
+        <h1>Your Notes</h1>
+
+        {!metamaskConnected ? (
+          <button onClick={connectToMetamask}>Connect to Metamask</button>
+        ) : (
+          <div>
+            <div>
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <button onClick={mintNote}>Mint Note</button>
+            </div>
+
+            {loading && <p>Loading...</p>}
+
+            <ul>
+              {notes.map((note) => (
+                <li key={note.id}>
+                  {note.text} - {ethers.utils.formatEther(note.price)} ETH
+                  {!note.purchased && (
+                    <button onClick={() => purchaseNote(note.id)}>
+                      Purchase
+                    </button>
+                  )}
+                </li>
+              ))}
+            </ul>
+
+            <p>
+              Connected to {account ? `account ${account}` : "Web3 provider"}
+            </p>
+            <p>Connected to {contract && `contract ${contract.address}`}</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Home;
